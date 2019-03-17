354458a7385bacde4c22f8e24413e587fa1d39d1
Andre Augusto <andre.9a@gmail.com>
2019-03-17 19:14:24 +0000

    final

--- main.c
+++ main.c
@@ -9,13 +9,17 @@ void readInput(Graph *graph);
 int* findSCC(Graph *graph, int* outputArray);
 int TarjanAdapt(Node *node, Node *nodesList);
 int min(int n1, int n2);
-int countArticulationPoints(Graph *graph);
-int iterateOverGraph(Node *node, Node *nodesList, int *visited);
+int findArticulationPoints(Graph *graph);
+int countArticulationPoints(Node *node, Node *nodesList, int *visited);
+int exploreArticulationPoints(Graph *graph);
+int exploreWay(Node *node, Node *nodesList, int *visitedCount);
 
 
-/* Size of output array in findScc */
-int SIZE = 25;
-int rootTreeEdges = 0;
+int SIZE = 25;								/* Size of output array in findScc */
+int rootTreeEdges = 0;						/* If the root has at least two tree edges then it is an articulation point */
+int auxCount = 0;
+int count = 0;
+int articulationPointId = 0;
 
 int main(){
 
@@ -46,7 +50,9 @@ int main(){
 
 	printf("\n");
 
-	printf("%d\n", countArticulationPoints(graph));
+	printf("%d\n", findArticulationPoints(graph));
+
+	printf("%d\n", exploreArticulationPoints(graph));
 
 	free(outputArray);
 
@@ -124,39 +130,39 @@ int TarjanAdapt(Node *node, Node *nodesList){
 	return 0;
 }
 
-int countArticulationPoints(Graph *graph){
+int findArticulationPoints(Graph *graph){
 
 	Node *nodesList = graph->nodesList;
 	int i = 0, totalArtPoints = 0, aux = 0;
 
-	int *visited = calloc((graph->numberRouters + 1), sizeof(int));
+	int *visitedCount = calloc((graph->numberRouters + 1), sizeof(int));			/* Used when exploring the graph to count the number of articulation points */
 
 	for (i = 1; i <= graph->numberRouters; i++){
-		if (visited[nodesList[i].id] == 0){
-			totalArtPoints = iterateOverGraph(&nodesList[i], nodesList, visited);
+		if (visitedCount[nodesList[i].id] == 0){
+			totalArtPoints = countArticulationPoints(&nodesList[i], nodesList, visitedCount);
 			rootTreeEdges = 0;
 		}
 	}
 
-	free(visited);
+	free(visitedCount);
 	return totalArtPoints;
 }
 
-int iterateOverGraph(Node *node, Node *nodesList, int *visited){
+int countArticulationPoints(Node *node, Node *nodesList, int *visitedCount){
 
-	static int numArticulationPoints = 0;			/* If the root has at least two tree edges then it is an articulation point */
-	int i = 0;
+	static int numArticulationPoints = 0;
+	int i = 0, countAux = 0;
 
-	visited[node->id] = 1;
+	visitedCount[node->id] = 1;
 	Neighbour *iter = node->first;
 
 	while(iter != NULL){
-		if (visited[iter->id] == 0){
+		if (visitedCount[iter->id] == 0	){
 			if ((node->discovered == node->low) && (rootTreeEdges != 2)){
 				rootTreeEdges++;
 			}
 
-			iterateOverGraph(&nodesList[iter->id], nodesList, visited);
+			countArticulationPoints(&nodesList[iter->id], nodesList, visitedCount);
 		}
 
 		if (nodesList[iter->id].low >= node->discovered){
@@ -171,8 +177,10 @@ int iterateOverGraph(Node *node, Node *nodesList, int *visited){
 
 			else {
 				if (rootTreeEdges == 2){
-					rootTreeEdges++;			/* Guarantees numArticulationPoints is only increased */
-					numArticulationPoints++;
+					if (node->articulationPoint != 1){
+						node->articulationPoint = 1;
+						numArticulationPoints++;
+					}
 				}
 			}
 		}
@@ -188,4 +196,51 @@ int min(int n1, int n2){
 		return n1;
 
 	return n2;
+}
+
+
+/* Returns the number of elements in the biggest subgraph that results in removing all articulation points */
+int exploreArticulationPoints(Graph *graph){
+
+	Node *nodesList = graph->nodesList;
+	
+	int *visitedCount = calloc(graph->numberRouters, sizeof(int));
+	int i = 0, maxAux = 0, max = 0;
+
+	for (i = 1; i <= graph->numberRouters; i++){
+		if ((visitedCount[nodesList[i].id] == 0) && (nodesList[i].articulationPoint == 1)){
+			articulationPointId = nodesList[i].id;
+			maxAux = exploreWay(&nodesList[i], nodesList, visitedCount);
+			if (maxAux > max)
+				max = maxAux;
+			auxCount = 0;
+		}
+	}
+
+	free(visitedCount);
+	return max;
+}
+
+
+int exploreWay(Node *node, Node *nodesList, int *visitedCount){
+
+	Neighbour *iter = node->first;
+	visitedCount[node->id] = 1;
+
+	while(iter != NULL){
+		if ((visitedCount[iter->id] != 1) && (nodesList[iter->id].articulationPoint == 0)){
+			auxCount++;
+			exploreWay(&nodesList[iter->id], nodesList, visitedCount);
+		}
+
+		if (iter->id == articulationPointId){
+			if (auxCount > count)
+				count = auxCount;
+		}
+
+		iter = iter->next;
+	}
+
+	auxCount = 0;
+	return count;
 }
